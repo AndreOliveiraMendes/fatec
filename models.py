c0a019bb7acc91a819da05c634c5d6fa0d743a35
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-06-10 16:36:51 -0300

    reduced req for now and updated model

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 import configparser
+from flask_sqlalchemy import SQLAlchemy
 from main import app
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 config_dict = {section: dict(config.items(section)) for section in config.sections()}
-config_dict['SECRET_KEY'] = config['DEFAULT']['SECRET_KEY']
 
-print("ok")
-print(config_dict)
+app.config['SQLALCHEMY_DATABASE_URI'] = config_dict['database']['mysql_url']
+app.config['SECRETS'] = config_dict['default']['secret_key']
+
+db = SQLAlchemy(app)
