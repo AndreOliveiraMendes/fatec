a73b424725b1835a7c34b66fd357c56de86ab1fe
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-06-12 23:37:02 -0300

    changed dynamic_redirect to accept a custom message

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ from route import *
 from models import *
 
 @app.template_global()
-def dynamic_redirect(seconds, url):
+def dynamic_redirect(seconds=5, message=None, target_url=None):
+    if message is None:
+        message = f"Você será redirecionado para a página inicial em ${{segundos}} segundo${{segundos === 1 ? '' : 's'}}."
+    if target_url is None:
+        target_url = url_for("home")
     script = f"""
     let segundos = {seconds};
     function iniciarTemporizador() {{
-        const elemento = document.getElementById("temporizador");
+        const elemento = document.getElementById("redirect-msg");
         const intervalo = setInterval(() => {{
             segundos--;
-            elemento.innerText = `Você será redirecionado para a página inicial em ${{segundos}} segundo${{segundos === 1 ? '' : 's'}}.`;
+            elemento.innerText = `{message}`;
             if (segundos <= 0) {{
                 clearInterval(intervalo);
-                window.location.href = "{url}";
+                window.location.href = "{target_url}";
             }}
         }}, 1000);
     }}
