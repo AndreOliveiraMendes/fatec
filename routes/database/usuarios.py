bbd3066208969548ec5c184038c71a8509a9eb4a
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-06-20 13:07:09 -0300

    removed usuarios to be developed later and moved some function to macro instead

--- routes/database/usuarios.py
+++ routes/database/usuarios.py
@@ -6,13 +6,5 @@ from decorators import admin_required
 @app.route("/admin/usuarios", methods=["GET", "POST"])
 @admin_required
 def gerenciar_usuarios():
-    acao = request.form.get('acao', 'abertura')
-    bloco = int(request.form.get('bloco', 0))
-    if request.method == 'POST':
-        extras = {}
-        if acao == 'listar':
-            usuarios = Usuarios.query.all()
-            extras['usarios'] = usuarios
-        return render_template("database/usuarios.html", acao=acao, bloco=bloco, **extras)
-    else:
-        return render_template("database/usuarios.html", acao=acao, bloco=bloco)
+    flash("Pagina em Desenvolvimento", "warning")
+    return redirect(url_for('under_dev_page'))
