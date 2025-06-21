17267d61b97d610750cafa37f0e5e2c6b3327e4b
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-06-21 06:21:04 -0300

    Atualizar o error.py
    updated to use perm from database

--- routes/error.py
+++ routes/error.py
@@ -5,18 +5,33 @@ from flask import flash, session, render_template
 def acesso_negado(e):
     flash(e, "danger")
     username = session.get('username', None)
-    perm = session.get('perm', None)
+    userid = session.get('userid')
+    perm = 0
+    if username:
+        user_perm:Usuarios_Permissao = Usuarios_Permissao.query.filter_by(id_permissao_usuario=userid).first()
+        if user_perm:
+            perm = user_perm.permissao
     return render_template("403.html", username=username, perm=perm), 403
 
 @app.errorhandler(404)
 def page_not_found(e):
     flash(e, "danger")
     username = session.get('username', None)
-    perm = session.get('perm', None)
+    userid = session.get('userid')
+    perm = 0
+    if username:
+        user_perm:Usuarios_Permissao = Usuarios_Permissao.query.filter_by(id_permissao_usuario=userid).first()
+        if user_perm:
+            perm = user_perm.permissao
     return render_template('404.html', username=username, perm=perm), 404
 
 @app.route('/under_dev')
 def under_dev_page():
     username = session.get('username', None)
-    perm = session.get('perm', None)
+    userid = session.get('userid')
+    perm = 0
+    if username:
+        user_perm:Usuarios_Permissao = Usuarios_Permissao.query.filter_by(id_permissao_usuario=userid).first()
+        if user_perm:
+            perm = user_perm.permissao
     return render_template('under_dev.html', username=username, perm=perm)
