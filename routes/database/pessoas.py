17ed1f2f4e4003f6db0fac30282bf7fb64b5163a
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-06-24 15:58:06 -0300

    added try catch

--- routes/database/pessoas.py
+++ routes/database/pessoas.py
@@ -1,6 +1,7 @@
 from main import app
 from flask import flash, session, render_template, request
 from datetime import datetime
+from sqlalchemy.exc import IntegrityError
 from models import db, Pessoas, Usuarios, Historicos
 from auxiliar.decorators import admin_required
 from auxiliar.auxiliar_routes import none_if_empty, get_query_params, get_user_info
@@ -51,15 +52,19 @@ def gerenciar_pessoas():
         elif acao == 'inserir' and bloco == 1:
             nome = none_if_empty(request.form.get('nome', None))
             email = none_if_empty(request.form.get('email', None))
-            nova_pessoa = Pessoas(nome_pessoa=nome, email_pessoa=email)
-            db.session.add(nova_pessoa)
-            historico = Historicos()
-            historico.id_pessoa = Usuarios.query.get(userid).id_usuario
-            historico.acao = f"inserindo a pessoa de id ({nova_pessoa.id_pessoa}) para nome:{nome} e email:{email}"
-            historico.dia = datetime.now()
-            db.session.add(historico)
-            db.session.commit()
-            flash("Pessoa cadastrada com sucesso", "success")
+            try:
+                nova_pessoa = Pessoas(nome_pessoa=nome, email_pessoa=email)
+                db.session.add(nova_pessoa)
+                historico = Historicos()
+                historico.id_pessoa = Usuarios.query.get(userid).id_usuario
+                historico.acao = f"[Inserção] Pessoa(ID: {nova_pessoa.id_pessoa}) - Nome: {nome}, Email: {email}"
+                historico.dia = datetime.now()
+                db.session.add(historico)
+                db.session.commit()
+                flash("Pessoa cadastrada com sucesso", "success")
+            except IntegrityError as e:
+                flash(f"Erro ao inserir pessoa: {str(e.orig)}", "danger")
+                db.session.rollback()
             bloco = 0
         elif acao == 'editar' and bloco == 0:
             pessoas_id_nome = db.session.query(Pessoas.id_pessoa, Pessoas.nome_pessoa).all()
@@ -80,15 +85,22 @@ def gerenciar_pessoas():
             pessoa = Pessoas.query.get(id_pessoa)
 
             if pessoa:
-                pessoa.nome_pessoa = nome
-                pessoa.email_pessoa = email
-                historico = Historicos()
-                historico.id_pessoa = Usuarios.query.get(userid).id_usuario
-                historico.acao = f"editado a pessoa de id ({pessoa.id_pessoa}) para nome:{nome} e email:{email}"
-                historico.dia = datetime.now()
-                db.session.add(historico)
-                db.session.commit()
-                flash("Pessoa atualizada com sucesso", "success")
+                try:
+                    pessoa.nome_pessoa = nome
+                    pessoa.email_pessoa = email
+
+                    historico = Historicos()
+                    historico.id_pessoa = Usuarios.query.get(userid).id_usuario
+                    historico.acao = f"[Edição] Pessoa(ID: {pessoa.id_pessoa}) - Novo Nome: {nome}, Novo Email: {email}"
+                    historico.dia = datetime.now()
+
+                    db.session.add(historico)
+                    db.session.commit()
+                    flash("Pessoa atualizada com sucesso", "success")
+
+                except IntegrityError as e:
+                    db.session.rollback()
+                    flash(f"Erro ao atualizar pessoa: {str(e.orig)}", "danger")
             else:
                 flash("Pessoa não encontrada", "danger")
 
@@ -105,14 +117,20 @@ def gerenciar_pessoas():
                 if user.id_pessoa == id_pessoa:
                     flash("Voce não pode se excluir", "danger")
                 else:
-                    historico = Historicos()
-                    historico.id_pessoa = Usuarios.query.get(userid).id_usuario
-                    historico.acao = f"excluindo a pessoa de id ({pessoa.id_pessoa}) para nome:{pessoa.nome_pessoa} e email:{pessoa.email_pessoa}"
-                    historico.dia = datetime.now()
-                    db.session.add(historico)
-                    db.session.delete(pessoa)
-                    db.session.commit()
-                    flash("Pessoa excluída com sucesso", "success")
+                    try:
+                        historico = Historicos()
+                        historico.id_pessoa = Usuarios.query.get(userid).id_usuario
+                        historico.acao = f"[Exclusão] Pessoa(ID: {pessoa.id_pessoa}) - Nome: {pessoa.nome_pessoa}, Email: {pessoa.email_pessoa}"
+                        historico.dia = datetime.now()
+
+                        db.session.delete(pessoa)
+                        db.session.add(historico)
+                        db.session.commit()
+                        flash("Pessoa excluída com sucesso", "success")
+
+                    except IntegrityError as e:
+                        db.session.rollback()
+                        flash(f"Erro ao excluir pessoa: {str(e.orig)}", "danger")
             else:
                 flash("Pessoa não encontrada", "danger")
 
