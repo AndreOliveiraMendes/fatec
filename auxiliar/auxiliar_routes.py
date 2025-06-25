996de2d631def0572cff1f147ddbb242b64b2f86
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-06-25 09:40:21 -0300

    added a casting when specified (default string)

--- auxiliar/auxiliar_routes.py
+++ auxiliar/auxiliar_routes.py
@@ -5,8 +5,11 @@ from sqlalchemy.inspection import inspect
 
 IGNORED_FORM_FIELDS = ['page', 'acao', 'bloco']
 
-def none_if_empty(value):
-    return value if value and value.strip() else None
+def none_if_empty(value, type='string'):
+    value = value if value and value.strip() else None
+    if value and type == 'int':
+        value = int(value)
+    return value
 
 def get_query_params(request):
     return {key: value for key, value in request.form.items() if key not in IGNORED_FORM_FIELDS}
