547a01b3230597f5d53bd3171691f147e7ab2313
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-06-25 14:44:28 -0300

    yet again added another param

--- routes/database/permissoes.py
+++ routes/database/permissoes.py
@@ -1,6 +1,6 @@
 from main import app
 from flask import flash, session, render_template, request
-from models import db, Permissoes
+from models import db, Permissoes, Usuarios, Pessoas
 from auxiliar.decorators import admin_required
 from auxiliar.auxiliar_routes import get_user_info
 
@@ -14,6 +14,14 @@ def gerenciar_permissoes():
     username, perm = get_user_info(userid)
     if request.method == 'POST':
         extras = {}
+        if acao == 'listar':
+            permissoes_paginadas = Permissoes.query.paginate(page=page, per_page=10, error_out=False)
+            extras['permissoes'] = permissoes_paginadas.items
+            extras['pagination'] = permissoes_paginadas
+            extras['userid'] = userid
+        elif acao == 'procurar' and bloco == 0:
+            result = db.session.query(Usuarios.id_usuario, Pessoas.nome_pessoa).join(Pessoas, Usuarios.id_pessoa == Pessoas.id_pessoa).all()
+            extras['results'] = result
         return render_template("database/permissoes.html", username=username, perm=perm, acao=acao, bloco=bloco, **extras)
     else:
         return render_template("database/permissoes.html", username=username, perm=perm, acao=acao, bloco=bloco)
