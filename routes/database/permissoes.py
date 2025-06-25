d3161cd4c061af9f27552e1684020022e2cb9f54
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-06-25 11:23:34 -0300

    added table permissoes

--- routes/database/permissoes.py
+++ routes/database/permissoes.py
@@ -1,10 +1,19 @@
 from main import app
-from flask import flash, session, render_template, request, redirect, url_for
+from flask import flash, session, render_template, request
 from models import db, Permissoes
 from auxiliar.decorators import admin_required
+from auxiliar.auxiliar_routes import get_user_info
 
-@app.route("/admin/permissoes")
+@app.route("/admin/permissoes", methods=["GET", "POST"])
 @admin_required
 def gerenciar_permissoes():
-    flash("Pagina em Desenvolvimento", "warning")
-    return redirect(url_for('under_dev_page'))
+    acao = request.form.get('acao', 'abertura')
+    bloco = int(request.form.get('bloco', 0))
+    page = int(request.form.get('page', 1))
+    userid = session.get('userid')
+    username, perm = get_user_info(userid)
+    if request.method == 'POST':
+        extras = {}
+        return render_template("database/permissoes.html", username=username, perm=perm, acao=acao, bloco=bloco, **extras)
+    else:
+        return render_template("database/permissoes.html", username=username, perm=perm, acao=acao, bloco=bloco)
