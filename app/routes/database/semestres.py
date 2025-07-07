ffe33b82e41e2d43bd262dcbd040e8204bd6b545
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-07 19:05:49 -0300

    search function finished

--- app/routes/database/semestres.py
+++ app/routes/database/semestres.py
@@ -20,4 +20,25 @@ def gerenciar_semestres():
             semestres_paginados = Semestres.query.paginate(page=page, per_page=10, error_out=False)
             extras['semestres'] = semestres_paginados.items
             extras['pagination'] = semestres_paginados
+        elif acao == 'procurar' and bloco == 1:
+            id_semestre = none_if_empty(request.form.get('id_semestre'), int)
+            data_inicio = none_if_empty(request.form.get('data_inicio'))
+            data_fim = none_if_empty(request.form.get('data_fim'))
+            filter = []
+            query_params = get_query_params(request)
+            query = Semestres.query
+            if id_semestre:
+                filter.append(Semestres.id_semestre == id_semestre)
+            if data_inicio:
+                filter.append(Semestres.data_inicio == data_inicio)
+            if data_fim:
+                filter.append(Semestres.data_fim == data_fim)
+            if not filter:
+                flash("especifique pelo menos um campo de busca", "danger")
+                bloco = 0
+            else:
+                semestres_paginados = query.filter(*filter).paginate(page=page, per_page=10, error_out=False)
+                extras['semestres'] = semestres_paginados.items
+                extras['pagination'] = semestres_paginados
+                extras['query_params'] = query_params
     return render_template("database/semestres.html", username=username, perm=perm, acao=acao, bloco=bloco, **extras)
