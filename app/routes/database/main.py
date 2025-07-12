dc611e727cd24f176053168c90a9240983f62539
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-12 06:41:11 -0300

    added a experimental schema writer (under dev)

--- app/routes/database/main.py
+++ app/routes/database/main.py
@@ -2,7 +2,8 @@ from flask import Blueprint, session, render_template
 from app.models import db
 from app.auxiliar.auxiliar_routes import get_user_info
 from app.auxiliar.decorators import admin_required
-from sqlalchemy import inspect
+from sqlalchemy import inspect, Table, MetaData
+from sqlalchemy.schema import CreateTable
 
 bp = Blueprint('main', __name__, url_prefix="/database")
 
@@ -18,6 +19,28 @@ def classification(obj):
         return 'warning'
     else:
         return 'danger'
+
+def get_topologic_sorted(fks):
+    result, sorted_tables = [], []
+    to_be_sorted = list(fks.keys())
+    interaction = 0
+    while len(to_be_sorted) > 0:
+        sorted_this_interaction = []
+        for table in to_be_sorted:
+            dependencies = fks[table]
+            if not dependencies or all(dep in sorted_tables for dep in dependencies):
+                sorted_this_interaction.append(table)
+        to_be_sorted = [table for table in to_be_sorted if table not in sorted_this_interaction]
+        for table in sorted_this_interaction:
+            sorted_tables.append(table)
+            result.append((table, interaction))
+        interaction += 1
+    return result
+
+def get_create_table(table):
+    metadata = MetaData()
+    tabela = Table(table, metadata, autoload_with=db.engine)
+    return CreateTable(tabela).compile(db.engine)
 
 @bp.route("/")
 @admin_required
@@ -37,3 +60,17 @@ def database():
     extras['chks'] = {table:inspector.get_check_constraints(table) for table in tables}
     extras['inds'] = {table:inspector.get_indexes(table) for table in tables}
     return render_template("database/database.html", username=username, perm=perm, **extras)
+
+@bp.route("/dump")
+@admin_required
+def dump():
+    userid = session.get('userid')
+    username, perm = get_user_info(userid)
+    extras = {}
+    inspector = inspect(db.engine)
+    tables = inspector.get_table_names()
+    fks = {table:[fk['referred_table'] for fk in inspector.get_foreign_keys(table)] for table in tables}
+    topologic_tables = get_topologic_sorted(fks)
+    extras['topologic_tables'] = [table_info + (get_create_table(table_info[0]),) for table_info in topologic_tables]
+
+    return render_template("database/dump.html", username=username, perm=perm, **extras)
