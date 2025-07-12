a5b2ead736b4d6bf969a920da6f600c656504e6c
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-12 14:11:04 -0300

    added a dump function

--- app/routes/database/main.py
+++ app/routes/database/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, render_template
+from flask import Blueprint, session, render_template, abort, Response
 from app.models import db
 from app.auxiliar.auxiliar_routes import get_user_info
 from app.auxiliar.decorators import admin_required
@@ -22,7 +22,7 @@ def classification(obj):
 
 def get_topologic_sorted(fks):
     result, sorted_tables = [], []
-    to_be_sorted = list(fks.keys())
+    to_be_sorted = [table for table in fks.keys()]
     interaction = 0
     while len(to_be_sorted) > 0:
         sorted_this_interaction = []
@@ -70,7 +70,46 @@ def dump():
     inspector = inspect(db.engine)
     tables = inspector.get_table_names()
     fks = {table:[fk['referred_table'] for fk in inspector.get_foreign_keys(table)] for table in tables}
-    topologic_tables = get_topologic_sorted(fks)
-    extras['topologic_tables'] = [table_info + (get_create_table(table_info[0]),) for table_info in topologic_tables]
+    topologic_tables = [table_info + (get_create_table(table_info[0]),) for table_info in get_topologic_sorted(fks)]
+    extras['topologic_tables'] = topologic_tables
 
     return render_template("database/dump.html", username=username, perm=perm, **extras)
+
+@bp.route("/dump/sql")
+@admin_required
+def dump_file():
+    inspector = inspect(db.engine)
+    tables = inspector.get_table_names()
+    fks = {table:[fk['referred_table'] for fk in inspector.get_foreign_keys(table)] for table in tables}
+    if has_cycle(fks):
+        abort(422, description="Não foi possível gerar o esquema: dependências cíclicas detectadas.")
+    else:
+        tables_creation_sql = [get_create_table(table_info[0]) for table_info in get_topologic_sorted(fks)]
+        conteudo = "\n\n".join(map(lambda c: str(c).strip().rstrip(";\n") + ";", tables_creation_sql))
+        return Response(
+            conteudo,
+            mimetype="text/plain; charset=utf-8",
+            headers={"Content-Disposition": "attachment;filename=schema.sql"}
+        )
+
+def dfs(table, fks, visited):
+    if table in visited:
+        return visited[table] == 0
+    else:
+        visited[table] = 0
+        for dep_table in fks[table]:
+            cycles = dfs(dep_table, fks,visited)
+            if cycles:
+                return True
+        visited[table] = 1
+        return False
+
+def has_cycle(fks):
+    cycles = False
+    visited = {}
+    for table in fks.keys():
+        if not table in visited:
+            cycles = dfs(table, fks, visited)
+        if cycles:
+            return True
+    return cycles
