740b7e534c62d6caf9fc7c9e00823a27765ab4ac
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-13 17:23:12 -0300

    updated exchemas

--- schema.sql
+++ schema.sql
@@ -1,146 +1,138 @@
 CREATE TABLE
-    IF NOT EXISTS `aulas` (
-        `id_aula` int NOT NULL AUTO_INCREMENT,
-        `horario_inicio` time NOT NULL,
-        `horario_fim` time NOT NULL,
-        PRIMARY KEY (`id_aula`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    aulas (
+        id_aula INTEGER NOT NULL AUTO_INCREMENT,
+        horario_inicio TIME NOT NULL,
+        horario_fim TIME NOT NULL,
+        PRIMARY KEY (id_aula)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `dias_semana` (
-        `id` INT (10) NOT NULL AUTO_INCREMENT,
-        `nome` VARCHAR(15) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-        PRIMARY KEY (`id`) USING BTREE,
-        UNIQUE INDEX `nome` (`nome`) USING BTREE
-    ) COLLATE = 'utf8mb4_0900_ai_ci' ENGINE = InnoDB;
+    dias_da_semana (
+        id INTEGER NOT NULL,
+        nome VARCHAR(15) NOT NULL,
+        PRIMARY KEY (id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `turnos` (
-        `id` INT (10) NOT NULL AUTO_INCREMENT,
-        `nome` VARCHAR(15) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-        `horario_inicio` TIME NOT NULL,
-        `horario_fim` TIME NOT NULL,
-        PRIMARY KEY (`id`) USING BTREE,
-        UNIQUE INDEX `nome` (`nome`) USING BTREE
-    ) COLLATE = 'utf8mb4_0900_ai_ci' ENGINE = InnoDB;
+    laboratorios (
+        id_laboratorio INTEGER NOT NULL AUTO_INCREMENT,
+        nome_laboratorio VARCHAR(100) NOT NULL,
+        disponibilidade ENUM ('DISPONIVEL', 'INDISPONIVEL') NOT NULL DEFAULT 'DISPONIVEL',
+        tipo ENUM ('LABORATORIO', 'SALA', 'EXTERNO') NOT NULL DEFAULT 'LABORATORIO',
+        PRIMARY KEY (id_laboratorio)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `aulas_ativas` (
-        `id_aula_ativa` INT (10) NOT NULL AUTO_INCREMENT,
-        `id_aula` INT (10) NOT NULL,
-        `inicio_ativacao` DATE NULL DEFAULT NULL,
-        `fim_ativacao` DATE NULL DEFAULT NULL,
-        `id_semana` INT (10) NULL DEFAULT NULL,
-        `id_turno` INT (10) NULL DEFAULT NULL,
-        `tipo_aula` ENUM ('AULA', 'EVENTO', 'OUTROS') NOT NULL DEFAULT 'AULA' COLLATE 'utf8mb4_0900_ai_ci',
-        PRIMARY KEY (`id_aula_ativa`) USING BTREE,
-        INDEX `id_aula` (`id_aula`) USING BTREE,
-        INDEX `id_semana` (`id_semana`) USING BTREE,
-        INDEX `id_turno` (`id_turno`) USING BTREE,
-        CONSTRAINT `aulas_ativas_ibfk_1` FOREIGN KEY (`id_aula`) REFERENCES `aulas` (`id_aula`) ON UPDATE NO ACTION ON DELETE NO ACTION,
-        CONSTRAINT `aulas_ativas_ibfk_2` FOREIGN KEY (`id_semana`) REFERENCES `dias_semana` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
-        CONSTRAINT `aulas_ativas_ibfk_3` FOREIGN KEY (`id_turno`) REFERENCES `turnos` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
-    ) COLLATE = 'utf8mb4_0900_ai_ci' ENGINE = InnoDB;
+    pessoas (
+        id_pessoa INTEGER NOT NULL AUTO_INCREMENT,
+        nome_pessoa VARCHAR(100) NOT NULL,
+        email_pessoa VARCHAR(100),
+        PRIMARY KEY (id_pessoa)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `laboratorios` (
-        `id_laboratorio` int NOT NULL AUTO_INCREMENT,
-        `nome_laboratorio` varchar(100) NOT NULL,
-        `disponibilidade` enum ('DISPONIVEL', 'INDISPONIVEL') NOT NULL DEFAULT 'DISPONIVEL',
-        `tipo` enum ('LABORATORIO', 'SALA', 'EXTERNO') NOT NULL DEFAULT 'LABORATORIO',
-        PRIMARY KEY (`id_laboratorio`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    semestres (
+        id_semestre INTEGER NOT NULL AUTO_INCREMENT,
+        nome_semestre VARCHAR(100) NOT NULL,
+        data_inicio DATE NOT NULL,
+        data_fim DATE NOT NULL,
+        PRIMARY KEY (id_semestre)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `pessoas` (
-        `id_pessoa` int NOT NULL AUTO_INCREMENT,
-        `nome_pessoa` varchar(100) NOT NULL,
-        `email_pessoa` varchar(100) DEFAULT NULL,
-        PRIMARY KEY (`id_pessoa`)
-    ) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    turnos (
+        id INTEGER NOT NULL AUTO_INCREMENT,
+        nome VARCHAR(15) NOT NULL,
+        horario_inicio TIME NOT NULL,
+        horario_fim TIME NOT NULL,
+        PRIMARY KEY (id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `usuarios` (
-        `id_usuario` int NOT NULL AUTO_INCREMENT,
-        `id_pessoa` int NOT NULL,
-        `tipo_pessoa` varchar(50) NOT NULL,
-        `situacao_pessoa` varchar(50) NOT NULL,
-        `grupo_pessoa` varchar(50) DEFAULT NULL,
-        PRIMARY KEY (`id_usuario`),
-        KEY `id_pessoa` (`id_pessoa`),
-        CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoas` (`id_pessoa`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    usuarios_especiais (
+        id_usuario_especial INTEGER NOT NULL AUTO_INCREMENT,
+        nome_usuario_especial VARCHAR(100) NOT NULL,
+        PRIMARY KEY (id_usuario_especial)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `historicos` (
-        `id_historico` int NOT NULL AUTO_INCREMENT,
-        `id_usuario` int NOT NULL,
-        `id_pessoa` int NOT NULL,
-        `tabela` varchar(100) DEFAULT NULL,
-        `categoria` varchar(100) DEFAULT NULL,
-        `data_hora` datetime NOT NULL,
-        `message` text NOT NULL,
-        `chave_primaria` text NOT NULL,
-        `observacao` text,
-        PRIMARY KEY (`id_historico`),
-        KEY `id_usuario` (`id_usuario`),
-        KEY `id_pessoa` (`id_pessoa`),
-        KEY `ix_historicos_tabela` (`tabela`),
-        KEY `ix_historicos_data_hora` (`data_hora`),
-        CONSTRAINT `historicos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-        CONSTRAINT `historicos_ibfk_2` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoas` (`id_pessoa`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    aulas_ativas (
+        id_aula_ativa INTEGER NOT NULL AUTO_INCREMENT,
+        id_aula INTEGER NOT NULL,
+        inicio_ativacao DATE,
+        fim_ativacao DATE,
+        id_semana INTEGER NOT NULL,
+        tipo_aula ENUM ('AULA', 'EVENTO', 'OUTROS') NOT NULL DEFAULT 'AULA',
+        PRIMARY KEY (id_aula_ativa),
+        CONSTRAINT aulas_ativas_ibfk_1 FOREIGN KEY (id_aula) REFERENCES aulas (id_aula),
+        CONSTRAINT aulas_ativas_ibfk_2 FOREIGN KEY (id_semana) REFERENCES dias_da_semana (id),
+        CONSTRAINT chk_inicio_menor_fim CHECK (
+            (
+                (`inicio_ativacao` is null)
+                or (`fim_ativacao` is null)
+                or (`inicio_ativacao` <= `fim_ativacao`)
+            )
+        )
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `permissoes` (
-        `id_permissao_usuario` int NOT NULL,
-        `permissao` int NOT NULL,
-        PRIMARY KEY (`id_permissao_usuario`),
-        CONSTRAINT `permissoes_ibfk_1` FOREIGN KEY (`id_permissao_usuario`) REFERENCES `usuarios` (`id_usuario`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    usuarios (
+        id_usuario INTEGER NOT NULL AUTO_INCREMENT,
+        id_pessoa INTEGER NOT NULL,
+        tipo_pessoa VARCHAR(50) NOT NULL,
+        situacao_pessoa VARCHAR(50) NOT NULL,
+        grupo_pessoa VARCHAR(50),
+        PRIMARY KEY (id_usuario),
+        CONSTRAINT usuarios_ibfk_1 FOREIGN KEY (id_pessoa) REFERENCES pessoas (id_pessoa)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `usuarios_especiais` (
-        `id_usuario_especial` int NOT NULL AUTO_INCREMENT,
-        `nome_usuario_especial` varchar(100) NOT NULL,
-        PRIMARY KEY (`id_usuario_especial`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    historicos (
+        id_historico INTEGER NOT NULL AUTO_INCREMENT,
+        id_usuario INTEGER,
+        tabela VARCHAR(100),
+        categoria VARCHAR(100),
+        data_hora DATETIME NOT NULL,
+        message TEXT NOT NULL,
+        chave_primaria TEXT NOT NULL,
+        observacao TEXT,
+        origem ENUM ('SISTEMA', 'USUARIO') NOT NULL DEFAULT 'SISTEMA',
+        PRIMARY KEY (id_historico),
+        CONSTRAINT historicos_ibfk_1 FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `semestres` (
-        `id_semestre` int NOT NULL AUTO_INCREMENT,
-        `data_inicio` date NOT NULL,
-        `data_fim` date NOT NULL,
-        PRIMARY KEY (`id_semestre`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    permissoes (
+        id_permissao_usuario INTEGER NOT NULL,
+        permissao INTEGER NOT NULL,
+        PRIMARY KEY (id_permissao_usuario),
+        CONSTRAINT permissoes_ibfk_1 FOREIGN KEY (id_permissao_usuario) REFERENCES usuarios (id_usuario)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-    IF NOT EXISTS `reservas_fixas` (
-        `id_reserva_fixa` int NOT NULL AUTO_INCREMENT,
-        `id_responsavel` int DEFAULT NULL,
-        `id_responsavel_especial` int DEFAULT NULL,
-        `tipo_responsavel` int NOT NULL,
-        `id_reserva_laboratorio` int NOT NULL,
-        `id_reserva_aula` int NOT NULL,
-        `status_reserva` int NOT NULL DEFAULT '0',
-        `tipo` int NOT NULL DEFAULT '0',
-        `id_reserva_semestre` int NOT NULL,
-        PRIMARY KEY (`id_reserva_fixa`),
-        UNIQUE KEY `uq_reserva_lab_aula_semestre` (
-            `id_reserva_laboratorio`,
-            `id_reserva_aula`,
-            `id_reserva_semestre`
-        ),
-        KEY `id_responsavel` (`id_responsavel`),
-        KEY `id_responsavel_especial` (`id_responsavel_especial`),
-        KEY `id_reserva_aula` (`id_reserva_aula`),
-        KEY `id_reserva_semestre` (`id_reserva_semestre`),
-        CONSTRAINT `reservas_fixas_ibfk_1` FOREIGN KEY (`id_responsavel`) REFERENCES `pessoas` (`id_pessoa`),
-        CONSTRAINT `reservas_fixas_ibfk_2` FOREIGN KEY (`id_responsavel_especial`) REFERENCES `usuarios_especiais` (`id_usuario_especial`),
-        CONSTRAINT `reservas_fixas_ibfk_3` FOREIGN KEY (`id_reserva_laboratorio`) REFERENCES `laboratorios` (`id_laboratorio`),
-        CONSTRAINT `reservas_fixas_ibfk_4` FOREIGN KEY (`id_reserva_aula`) REFERENCES `aulas_ativas` (`id_aula_ativa`),
-        CONSTRAINT `reservas_fixas_ibfk_5` FOREIGN KEY (`id_reserva_semestre`) REFERENCES `semestres` (`id_semestre`),
-        CONSTRAINT `check_tipo_responsavel` CHECK (
+    reservas_fixas (
+        id_reserva_fixa INTEGER NOT NULL AUTO_INCREMENT,
+        id_responsavel INTEGER,
+        id_responsavel_especial INTEGER,
+        tipo_responsavel INTEGER NOT NULL,
+        id_reserva_laboratorio INTEGER NOT NULL,
+        id_reserva_aula INTEGER NOT NULL,
+        id_reserva_semestre INTEGER NOT NULL,
+        tipo_reserva ENUM (
+            'GRADUACAO',
+            'ESPECIALIZACAO',
+            'EAD',
+            'NAPTI',
+            'CURSO',
+            'USO_DOS_ALUNOS'
+        ) NOT NULL DEFAULT 'GRADUACAO',
+        PRIMARY KEY (id_reserva_fixa),
+        CONSTRAINT reservas_fixas_ibfk_1 FOREIGN KEY (id_responsavel) REFERENCES pessoas (id_pessoa),
+        CONSTRAINT reservas_fixas_ibfk_2 FOREIGN KEY (id_responsavel_especial) REFERENCES usuarios_especiais (id_usuario_especial),
+        CONSTRAINT reservas_fixas_ibfk_3 FOREIGN KEY (id_reserva_laboratorio) REFERENCES laboratorios (id_laboratorio),
+        CONSTRAINT reservas_fixas_ibfk_4 FOREIGN KEY (id_reserva_aula) REFERENCES aulas_ativas (id_aula_ativa),
+        CONSTRAINT reservas_fixas_ibfk_5 FOREIGN KEY (id_reserva_semestre) REFERENCES semestres (id_semestre),
+        CONSTRAINT check_tipo_responsavel CHECK (
             (
                 (
                     (`tipo_responsavel` = 0)
@@ -158,5 +150,12 @@ CREATE TABLE
                     and (`id_responsavel_especial` is not null)
                 )
             )
+        ),
+        CONSTRAINT check_tipo_responsavel_value CHECK (
+            (
+                (`tipo_responsavel` = 0)
+                or (`tipo_responsavel` = 1)
+                or (`tipo_responsavel` = 2)
+            )
         )
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_0900_ai_ci;
