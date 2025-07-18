dedbebdb617ad3e93de97db4b61f13434ca6f00a
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-18 04:25:03 -0300

    updated semestres

--- app/auxiliar/dao.py
+++ app/auxiliar/dao.py
@@ -1,5 +1,5 @@
 from sqlalchemy import select
-from app.models import db, Pessoas, Usuarios, Usuarios_Especiais, Aulas, Laboratorios
+from app.models import db, Pessoas, Usuarios, Usuarios_Especiais, Aulas, Laboratorios, Semestres
 
 #pessoas
 def get_pessoas(acao = None, userid = None):
@@ -8,19 +8,19 @@ def get_pessoas(acao = None, userid = None):
         user = db.session.get(Usuarios, userid)
         if acao == 'excluir' and user:
             spin = spin.where(Pessoas.id_pessoa != user.id_usuario)
-    return db.session.execute(spin).scalars().all()
+    return db.session.execute(spin).all()
 
 #usuarios
 def get_usuarios(acao = None, userid = None):
     suin = select(Usuarios.id_usuario, Pessoas.nome_pessoa).join(Pessoas)
     if acao == 'excluir' and userid is not None:
         suin = suin.where(Usuarios.id_usuario != userid)
-    return db.session.execute(suin).scalars().all()
+    return db.session.execute(suin).all()
 
 #usuarios especiais
 def get_usuarios_especiais():
-    suein = select(Usuarios_Especiais)
-    return db.session.execute(suein).scalars().all()
+    sue = select(Usuarios_Especiais)
+    return db.session.execute(sue).scalars().all()
 
 #aulas
 def get_aulas():
@@ -29,5 +29,10 @@ def get_aulas():
 
 #laboratorios
 def get_laboratorios():
-    slin = select(Laboratorios)
-    return db.session.execute(slin).scalars().all()
+    slin = select(Laboratorios.id_laboratorio, Laboratorios.nome_laboratorio)
+    return db.session.execute(slin).all()
+
+#semestre
+def get_semestre():
+    ssin = select(Semestres.id_semestre, Semestres.nome_semestre)
+    return db.session.execute(ssin).all()
--- app/routes/database/semestres.py
+++ app/routes/database/semestres.py
@@ -1,17 +1,16 @@
 import copy
-from flask import Blueprint
-from flask import flash, session, render_template, request
+from flask import Blueprint, flash, session, render_template, request
+from flask_sqlalchemy.pagination import SelectPagination
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError, OperationalError
 from config.general import PER_PAGE
 from app.models import db, Semestres
 from app.auxiliar.decorators import admin_required
 from app.auxiliar.auxiliar_routes import none_if_empty, parse_date_string, get_user_info, \
     get_query_params, registrar_log_generico_usuario, get_session_or_request, register_return
+from app.auxiliar.dao import get_semestre
 
 bp = Blueprint('semestres', __name__, url_prefix="/database")
-
-def get_semestre():
-    return db.session.query(Semestres.id_semestre, Semestres.nome_semestre).all()
 
 @bp.route("/semestres", methods=["GET", "POST"])
 @admin_required
@@ -25,7 +24,8 @@ def gerenciar_semestres():
     extras = {}
     if request.method == 'POST':
         if acao == 'listar':
-            semestres_paginados = Semestres.query.paginate(page=page, per_page=PER_PAGE, error_out=False)
+            ss = select(Semestres)
+            semestres_paginados = SelectPagination(select=ss, session=db.session, page=page, per_page=PER_PAGE, error_out=False)
             extras['semestres'] = semestres_paginados.items
             extras['pagination'] = semestres_paginados
 
@@ -37,7 +37,6 @@ def gerenciar_semestres():
             data_fim = parse_date_string(request.form.get('data_fim'))
             filter = []
             query_params = get_query_params(request)
-            query = Semestres.query
             if id_semestre is not None:
                 filter.append(Semestres.id_semestre == id_semestre)
             if nome_semestre:
@@ -50,7 +49,8 @@ def gerenciar_semestres():
             if data_fim:
                 filter.append(Semestres.data_fim == data_fim)
             if filter:
-                semestres_paginados = query.filter(*filter).paginate(page=page, per_page=PER_PAGE, error_out=False)
+                ssf = select(Semestres).where(*filter)
+                semestres_paginados = semestres_paginados = SelectPagination(select=ssf, session=db.session, page=page, per_page=PER_PAGE, error_out=False)
                 extras['semestres'] = semestres_paginados.items
                 extras['pagination'] = semestres_paginados
                 extras['query_params'] = query_params
@@ -70,8 +70,8 @@ def gerenciar_semestres():
                 db.session.commit()
                 flash("Semestre cadastrado com sucesso", "success")
             except (IntegrityError, OperationalError) as e:
-                flash(f"Erro ao cadastrar semestre:{str(e.orig)}")
                 db.session.rollback()
+                flash(f"Erro ao cadastrar semestre:{str(e.orig)}")
 
             redirect_action, bloco = register_return('semestres.gerenciar_semestres', acao, extras)
 
@@ -79,14 +79,14 @@ def gerenciar_semestres():
             extras['semestres'] = get_semestre()
         elif acao in ['editar', 'excluir'] and bloco == 1:
             id_semestre = none_if_empty(request.form.get('id_semestre'), int)
-            semestre = Semestres.query.get_or_404(id_semestre)
+            semestre = db.get_or_404(Semestres, id_semestre)
             extras['semestre'] = semestre
         elif acao == 'editar' and bloco == 2:
             id_semestre = none_if_empty(request.form.get('id_semestre'), int)
             nome_semestre = none_if_empty(request.form.get('nome_semestre'))
             data_inicio = parse_date_string(request.form.get('data_inicio'))
             data_fim = parse_date_string(request.form.get('data_fim'))
-            semestre = Semestres.query.get_or_404(id_semestre)
+            semestre = db.get_or_404(Semestres, id_semestre)
             try:
                 dados_anteriores = copy.copy(semestre)
                 semestre.nome_semestre = nome_semestre
@@ -106,7 +106,7 @@ def gerenciar_semestres():
         elif acao == 'excluir' and bloco == 2:
             id_semestre = none_if_empty(request.form.get('id_semestre'), int)
 
-            semestre = Semestres.query.get_or_404(id_semestre)
+            semestre = db.get_or_404(Semestres, id_semestre)
             try:
                 db.session.delete(semestre)
 
