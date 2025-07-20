9c1a219b56893d319d21c83a26a9929689447b27
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-20 04:51:09 -0300

    removed some imports for now

--- app/routes/database/setup.py
+++ app/routes/database/setup.py
@@ -1,10 +1,6 @@
-from flask import Blueprint, session, render_template, abort, Response
-from app.models import db
+from flask import Blueprint, session, render_template
 from app.auxiliar.auxiliar_routes import get_user_info
 from app.auxiliar.decorators import admin_required
-from sqlalchemy import inspect, Table, MetaData, UniqueConstraint
-from sqlalchemy.schema import CreateTable
-from sqlalchemy.dialects import mysql
 
 bp = Blueprint('setup', __name__, url_prefix="/database/fast_setup/")
 
