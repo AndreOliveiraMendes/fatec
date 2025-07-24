75a83c6bf97943bd305f23ff1b0c335876593181
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-24 20:11:01 -0300

    added failback for no period configured

--- app/routes/reserva_fixa/reserva_fixa.py
+++ app/routes/reserva_fixa/reserva_fixa.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, flash, session, render_template, redirect, url_for, request
 from sqlalchemy import select
 from datetime import date, datetime
-from app.models import db, Semestres
+from app.models import db, Semestres, Turnos
 from app.auxiliar.auxiliar_routes import get_user_info
 
 bp = Blueprint('reservas_semanais', __name__, url_prefix="/reserva_fixa")
@@ -37,6 +37,12 @@ def get_semestre(id_semestre):
     username, perm = get_user_info(userid)
     semestre = db.get_or_404(Semestres, id_semestre)
     extras = {'semestre':semestre}
+    sel_turnos = select(Turnos).order_by(Turnos.horario_inicio)
+    turnos = db.session.execute(sel_turnos).scalars().all()
+    if len(turnos) == 0:
+        flash("cadastre ao menos 1 turno", "danger")
+        return redirect(url_for('default.home'))
     now = datetime.now()
+    extras['turnos'] = turnos
     extras['time'] = now.time()
     return render_template('reserva_fixa/semestre.html', username=username, perm=perm, **extras)
