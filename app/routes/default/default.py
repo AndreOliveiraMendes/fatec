56789386b608416d0071d3917f56c79509e88229
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-25 23:52:31 -0300

    added a item on nav menu to check reservas (wip)

--- app/routes/default/default.py
+++ app/routes/default/default.py
@@ -1,5 +1,4 @@
-from flask import Blueprint
-from flask import session, render_template
+from flask import Blueprint, session, render_template
 from app.models import db, Usuarios
 from app.auxiliar.auxiliar_routes import get_user_info
 from app.auxiliar.decorators import login_required
@@ -11,14 +10,6 @@ def home():
     userid = session.get('userid')
     username, perm = get_user_info(userid)
     return render_template("homepage.html", username=username, perm=perm)
-
-@bp.route("/perfil")
-@login_required
-def perfil():
-    userid = session.get('userid')
-    user = db.session.get(Usuarios, userid)
-    username, perm = get_user_info(userid)
-    return render_template("usuario/perfil.html", username=username, perm=perm, usuario=user)
 
 @bp.route('/under_dev')
 def under_dev_page():
---
+++ app/routes/user/usuario.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+from flask import session, render_template
+from app.models import db, Usuarios
+from app.auxiliar.auxiliar_routes import get_user_info
+from app.auxiliar.decorators import login_required
+
+bp = Blueprint('usuario', __name__, url_prefix='/usuario')
+
+@bp.route("/perfil")
+@login_required
+def perfil():
+    userid = session.get('userid')
+    user = db.session.get(Usuarios, userid)
+    username, perm = get_user_info(userid)
+    return render_template("usuario/perfil.html", username=username, perm=perm, usuario=user)
+
+@bp.route("/reservas")
+@login_required
+def veficiar_reservas():
+    userid = session.get('userid')
+    username, perm = get_user_info(userid)
+    return render_template("usuario/reserva.html", username=username, perm=perm)
