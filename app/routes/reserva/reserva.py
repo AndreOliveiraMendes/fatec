1ce567636ad9e7efb27223007f037d0330ed6562
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-07-30 16:57:04 -0300

    consultar reservas (wip)

--- app/routes/reserva/reserva.py
+++ app/routes/reserva/reserva.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, session
-from app.auxiliar.auxiliar_routes import get_user_info
+from flask import Blueprint, render_template, session, request
+from app.auxiliar.auxiliar_routes import get_user_info, parse_date_string
 from datetime import datetime
 from config.general import LOCAL_TIMEZONE
-from app.auxiliar.dao import get_laboratorios
+from app.models import TipoAulaEnum
+from app.auxiliar.dao import get_laboratorios, get_turnos
 
 bp = Blueprint('consultar_reservas', __name__, url_prefix="/consultar_reserva")
 
@@ -12,5 +13,13 @@ def main_page():
     username, perm = get_user_info(userid)
     today = datetime.now(LOCAL_TIMEZONE)
     extras = {'dia':today}
+    reserva_dia = request.args.get('reserva-dia', default=today.date().strftime("%Y-%m-%d"))
+    reserva_turno = request.args.get('reserva_turno', type=int)
+    reserva_tipo_horario = request.args.get('reserva_tipo_horario')
     extras['laboratorios'] = get_laboratorios(True, True)
+    extras['turnos'] = get_turnos()
+    extras['tipo_aula'] = TipoAulaEnum
+    extras['reserva_dia'] = parse_date_string(reserva_dia)
+    extras['reserva_turno'] = reserva_turno
+    extras['reserva_tipo_horario'] = reserva_tipo_horario
     return render_template("reserva/main.html", username=username, perm=perm, **extras)
