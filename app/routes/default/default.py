13a4d2f976a36bfb8f6475b8e20e9f7ce965e252
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-09-02 16:23:00 -0300

    added favicon routes

--- app/routes/default/default.py
+++ app/routes/default/default.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, session, url_for
+from flask import Blueprint, redirect, render_template, session, url_for, send_from_directory
 
 from app.auxiliar.auxiliar_routes import get_user_info
 
@@ -19,3 +19,16 @@ def under_dev_page():
     userid = session.get('userid')
     username, perm = get_user_info(userid)
     return render_template('under_dev.html', username=username, perm=perm)
+
+@bp.route('/favicon.svg')
+def favicon_svg():
+    return send_from_directory('static/images', 'favicon.svg')
+
+@bp.route('/favicon.png')
+def favicon_png():
+    return send_from_directory('static/images', 'favicon.png')
+
+@bp.route('/favicon.ico')
+def favicon_ico():
+    return send_from_directory('static/images', 'favicon.ico')
+
