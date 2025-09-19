818f01ad0931ff3fbbc2471f5808c19af74ac090
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-09-19 17:19:30 -0300

    done with search function

--- app/routes/database/reservas_auditorios.py
+++ app/routes/database/reservas_auditorios.py
@@ -6,7 +6,10 @@ from sqlalchemy import select
 from sqlalchemy.exc import (DataError, IntegrityError, InterfaceError,
                             InternalError, OperationalError, ProgrammingError)
 
-from app.auxiliar.auxiliar_routes import register_return, registrar_log_generico_usuario, get_session_or_request, get_user_info, none_if_empty, parse_date_string
+from app.auxiliar.auxiliar_routes import (get_query_params, get_session_or_request,
+                                          get_user_info, none_if_empty,
+                                          parse_date_string, register_return,
+                                          registrar_log_generico_usuario)
 from app.auxiliar.dao import get_aulas_ativas, get_locais, get_pessoas
 from app.auxiliar.decorators import admin_required
 from app.models import Reservas_Auditorios, StatusReservaAuditorioEnum, db
@@ -36,8 +39,54 @@ def gerenciar_reservas_auditorios():
             extras['reservas_auditorios'] = reservas_auditorios_paginadas.items
             extras['pagination'] = reservas_auditorios_paginadas
 
-        elif acao == 'procurar':
-            pass
+        elif acao == 'procurar' and bloco == 0:
+            extras['pessoas'] = get_pessoas()
+            extras['locais'] = get_locais()
+            extras['aulas_ativas'] = get_aulas_ativas()
+        elif acao == 'procurar' and bloco == 1:
+            id_reserva_auditorio = none_if_empty(request.form.get('id_reserva_auditorio'), int)
+            id_responsavel = none_if_empty(request.form.get('id_responsavel'), int)
+            id_reserva_local = none_if_empty(request.form.get('id_reserva_local'), int)
+            id_reserva_aula = none_if_empty(request.form.get('id_reserva_aula'), int)
+            dia_reserva = parse_date_string(request.form.get('dia_reserva'))
+            status_reserva = none_if_empty(request.form.get('status_reserva'),)
+            id_autorizador = none_if_empty(request.form.get('id_autorizador'), int)
+            observação_responsavel = none_if_empty(request.form.get('observação_responsavel'))
+            observação_autorizador = none_if_empty(request.form.get('observação_autorizador'))
+            filter = []
+            query_params = get_query_params(request)
+            if id_reserva_auditorio is not None:
+                filter.append(Reservas_Auditorios.id_reserva_auditorio == id_reserva_auditorio)
+            if id_responsavel is not None:
+                filter.append(Reservas_Auditorios.id_responsavel == id_responsavel)
+            if id_reserva_local is not None:
+                filter.append(Reservas_Auditorios.id_reserva_local == id_reserva_local)
+            if id_reserva_aula is not None:
+                filter.append(Reservas_Auditorios.id_reserva_aula == id_reserva_aula)
+            if dia_reserva:
+                filter.append(Reservas_Auditorios.dia_reserva == dia_reserva)
+            if status_reserva:
+                filter.append(Reservas_Auditorios.status_reserva == StatusReservaAuditorioEnum(status_reserva))
+            if id_autorizador is not None:
+                filter.append(Reservas_Auditorios.id_autorizador == id_autorizador)
+            if observação_responsavel:
+                filter.append(Reservas_Auditorios.observação_responsavel.ilike(f"%{observação_responsavel}%"))
+            if observação_autorizador:
+                filter.append(Reservas_Auditorios.observação_autorizador.ilike(f"%{observação_autorizador}"))
+            if filter:
+                sel_reservas = select(Reservas_Auditorios).where(*filter)
+                reservas_auditorios_paginadas = SelectPagination(
+                select=sel_reservas, session=db.session,
+                page=page, per_page=PER_PAGE, error_out=False
+            )
+                extras['reservas_auditorios'] = reservas_auditorios_paginadas.items
+                extras['pagination'] = reservas_auditorios_paginadas
+                extras['query_params'] = query_params
+            else:
+                flash("especifique ao menos um campo", "danger")
+                redirect_action, bloco = register_return(url, acao, extras,
+                    pessoas=get_pessoas(), locais=get_locais(), aulas_ativas=get_aulas_ativas()
+            )
 
         elif acao == 'inserir' and bloco == 0:
             extras['pessoas'] = get_pessoas()
