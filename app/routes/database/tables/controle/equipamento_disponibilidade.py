b978bf307f59e84650124645eb53dc9960c5ec3d
Andre Oliveira Mendes <ao_mendes@hotmail.com>
2026-03-09 16:47:00 -0300

    feat: enhance search functionality for equipment availability with additional filters

--- app/routes/database/tables/controle/equipamento_disponibilidade.py
+++ app/routes/database/tables/controle/equipamento_disponibilidade.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from flask import Blueprint, flash, render_template, request, session
 from flask_sqlalchemy.pagination import SelectPagination
-from sqlalchemy import select
+from sqlalchemy import or_, select
 
 from app.auxiliar.constant import DB_ERRORS
 from app.auxiliar.general import none_if_empty
@@ -15,7 +15,7 @@ from app.dao.internal.usuarios import get_user
 from app.decorators.decorators import admin_required
 from app.extensions import db
 from app.models.controle import EquipamentoDisponibilidade
-from app.routes_helper.request import get_session_or_request
+from app.routes_helper.request import get_query_params, get_session_or_request
 from config.database_views import get_url
 from config.general import PER_PAGE
 
@@ -44,13 +44,62 @@ def gerenciar_equipamentos_disponibilidade():
 
         elif acao == "procurar" and bloco == 0:
             extras["equipamentos"] = get_equipamentos()
+        elif acao == "procurar" and bloco == 1:
+            id_disponibilidade = none_if_empty(request.form.get('id_disponibilidade'), int)
+            equipamento = none_if_empty(request.form.get('id_equipamento'), int)
+            data_start = parse_date_string(request.form.get('data_start'))
+            data_end = parse_date_string(request.form.get('data_end'))
+            quantidade_min = none_if_empty(request.form.get('quantidade_min'), int)
+            quantidade_max = none_if_empty(request.form.get('quantidade_max'), int)
+
+            filters = []
+            query_params = get_query_params(request)
+            if id_disponibilidade is not None:
+                filters.append(EquipamentoDisponibilidade.id_disponibilidade == id_disponibilidade)
+            if equipamento is not None:
+                filters.append(EquipamentoDisponibilidade.id_equipamento == equipamento)
+            if data_start:
+                filters.append(
+                    or_(
+                        EquipamentoDisponibilidade.gerado_em >= data_start,
+                        EquipamentoDisponibilidade.atualizado_em >= data_start
+                    )
+                )
+            if data_end:
+                filters.append(
+                    or_(
+                        EquipamentoDisponibilidade.gerado_em <= data_end,
+                        EquipamentoDisponibilidade.atualizado_em <= data_end
+                    )
+                )
+            if quantidade_min:
+                filters.append(EquipamentoDisponibilidade.quantidade_total >= quantidade_min)
+            if quantidade_max:
+                filters.append(EquipamentoDisponibilidade.quantidade_total <= quantidade_max)
+            if filters:
+                sel_disponibilidade = select(EquipamentoDisponibilidade).where(
+                    *filters
+                )
+                disponibilidade_paginada = SelectPagination(
+                    select=sel_disponibilidade, session=db.session,
+                    page=page, per_page=PER_PAGE, error_out=False
+                )
+                extras['disponibilidades'] = disponibilidade_paginada.items
+                extras['pagination'] = disponibilidade_paginada
+                extras['query_params'] = query_params
+            else:
+                flash("especifique ao menos um campo", "danger")
+                redirect_action, bloco = register_return(
+                    url, acao, extras,
+                    equipamentos = get_equipamentos()
+                )
 
         elif acao == "inserir" and bloco == 0:
             extras["equipamentos"] = get_equipamentos()
         elif acao == "inserir" and bloco == 1:
-            id_equipamento = none_if_empty(request.form.get('id_equipamento'))
+            id_equipamento = none_if_empty(request.form.get('id_equipamento'), int)
             data = parse_date_string(request.form.get('data'))
-            quantidade_total = none_if_empty(request.form.get('quantidade_total'))
+            quantidade_total = none_if_empty(request.form.get('quantidade_total'), int)
 
             try:
                 novo_registro_disponibilidade = EquipamentoDisponibilidade(
