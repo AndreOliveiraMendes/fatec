aaa041c596cb9dd7943e7948e31fd4ee857a294f
Andre Oliveira Mendes <ao_mendes@hotmail.com>
2026-03-11 16:59:49 -0300

    Movimentações de Equipamentos (wip)

---
+++ app/routes/database/tables/controle/movimentacoes_equipamento/route.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, g, render_template, request
+
+from app.decorators.decorators import admin_required, crud_route
+from app.routes_helper.controller import get_controller
+
+from .handlers import dispatcher
+from .states import VALID_STATES
+
+bp = Blueprint('database_movimentacoes_equipamento', __name__, url_prefix="/database")
+
+@bp.route("/movimentacoes_equipamento", methods=["GET", "POST"])
+@admin_required
+@crud_route()
+def gerenciar_movimentacoes_equipamento():
+    if request.method == 'POST':
+        get_controller(VALID_STATES, dispatcher, g.acao, g.bloco)
+
+    if g.redirect_action:
+        return g.redirect_action
+    return render_template("database/table/movimentacoes_equipamento.html", user=g.user, acao=g.acao, bloco=g.bloco, **g.extras)
