84f8aef0ac1dc8bcc490405764265c16a90e9fa1
AndreOliveiraMendes <ao_mendes@hotmail.com>
2026-03-11 18:24:55 -0300

    prepared schema for implementation

--- app/routes/database/tables/controle/movimentacoes_equipamento/handlers.py
+++ app/routes/database/tables/controle/movimentacoes_equipamento/handlers.py
@@ -17,3 +17,25 @@ def search_fetch():
 @register_handler(dispatcher, 'inserir', 0)
 def insert_prefetch():
     pass
+
+@register_handler(dispatcher, 'inserir', 1)
+def insert_push():
+    pass
+
+@register_handler(dispatcher, 'edit', 0)
+@register_handler(dispatcher, 'excluir', 0)
+def fetch_movimentacoes():
+    pass
+
+@register_handler(dispatcher, 'edit', 1)
+@register_handler(dispatcher, 'excluir', 1)
+def fetch_movimentacao():
+    pass
+
+@register_handler(dispatcher, 'edit', 2)
+def edit_push():
+    pass
+
+@register_handler(dispatcher, 'excluir', 2)
+def delete_push():
+    pass
