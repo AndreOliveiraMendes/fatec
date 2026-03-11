3a42d0fe305115ce6ad36e1b342f31dad710bedf
AndreOliveiraMendes <ao_mendes@hotmail.com>
2026-03-11 18:17:59 -0300

    added schema

--- app/routes/database/tables/controle/movimentacoes_equipamento/handlers.py
+++ app/routes/database/tables/controle/movimentacoes_equipamento/handlers.py
@@ -1,1 +1,19 @@
+from app.decorators.decorators import register_handler
+
 dispatcher = {}
+
+@register_handler(dispatcher, 'listar', 0)
+def list_handler():
+    pass
+
+@register_handler(dispatcher, 'procurar', 0)
+def search_prefetch():
+    pass
+
+@register_handler(dispatcher, 'procurar', 1)
+def search_fetch():
+    pass
+
+@register_handler(dispatcher, 'inserir', 0)
+def insert_prefetch():
+    pass
