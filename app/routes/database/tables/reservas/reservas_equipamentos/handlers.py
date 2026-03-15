6d4a0f7db89126826f1ad814bb77de50ead51140
AndreOliveiraMendes <ao_mendes@hotmail.com>
2026-03-14 22:18:15 -0300

    feat: list method for reservas_equipamentos

--- app/routes/database/tables/reservas/reservas_equipamentos/handlers.py
+++ app/routes/database/tables/reservas/reservas_equipamentos/handlers.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
 from flask import g, request
+from flask_sqlalchemy.pagination import SelectPagination
+from sqlalchemy import select
 
 from app.auxiliar.general import none_if_empty
 from app.auxiliar.navigation import register_return
-from app.auxiliar.parsing import parse_date_string
+from app.auxiliar.parsing import parse_date_string, parse_datetime_string
 from app.dao.internal.aulas import get_aulas_ativas
 from app.dao.internal.usuarios import get_pessoas
 from app.decorators.decorators import register_handler
@@ -12,12 +14,19 @@ from app.enums import StatusReservaEquipamentoEnum
 from app.extensions import db
 from app.models.reservas.reservas_equipamentos import Reservas_Equipamentos
 from app.routes_helper.db_actions import db_action
+from config.general import PER_PAGE
 
 dispatcher = {}
 
 @register_handler(dispatcher, 'listar', 0)
 def list_handler():
-    pass
+    sel_reservas = select(Reservas_Equipamentos)
+    reservas_paginadas = SelectPagination(
+        select=sel_reservas, session=db.session,
+        page=g.page, per_page=PER_PAGE, error_out=False
+    )
+    g.extras['reservas_equipamentos'] = reservas_paginadas.items
+    g.extras['pagination'] = reservas_paginadas
 
 @register_handler(dispatcher, 'procurar', 0)
 def search_prefetch():
@@ -39,7 +48,9 @@ def insert_push():
     id_reserva_responsavel = none_if_empty(request.form.get('id_reserva_responsavel'), int)
     data_reserva = parse_date_string(request.form.get('data_reserva'))
     cancelado_por_id = none_if_empty(request.form.get('cancelado_por_id'), int)
+    cancelado_em = parse_datetime_string(request.form.get('cancelado_em'))
     estado = none_if_empty(request.form.get('estado'))
+    motivo_cancelamento = none_if_empty(request.form.get('motivo_cancelamento'))
 
     nova_reserva = Reservas_Equipamentos(
         id_reserva_aula = id_reserva_aula,
@@ -49,18 +60,28 @@ def insert_push():
 
     def inserir():
         nova_reserva.estado = StatusReservaEquipamentoEnum(estado)
-        if cancelado_por_id:
+        if nova_reserva.estado == StatusReservaEquipamentoEnum.CANCELADA:
+            if not cancelado_por_id:
+                raise ValueError("Cancelamento precisa de responsável")
             nova_reserva.cancelado_por_id = cancelado_por_id
-            nova_reserva.cancelado_em = datetime.now()
-
+            if cancelado_em:
+                nova_reserva.cancelado_em = cancelado_em
+            else:
+                nova_reserva.cancelado_em = datetime.now()
+            nova_reserva.motivo_cancelamento = motivo_cancelamento
         db.session.add(nova_reserva)
+
+    def post_inserir():
+        if nova_reserva.cancelado_em and nova_reserva.cancelado_em < nova_reserva.criado_em:
+            raise ValueError("Cancelamento não pode ser antes da criação")
 
     db_action(
         "Inserção",
         "Reserva inserida com sucesso",
         "Erro ao inserir reserva",
         obj=nova_reserva,
-        action=inserir
+        action=inserir,
+        post_action=post_inserir
     )
 
     g.redirect_action, g.bloco = register_return(
