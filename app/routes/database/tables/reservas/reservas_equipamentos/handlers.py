7fa3d1b87b51fc5cf6d081b43183ca7d64419ef6
Andre Oliveira Mendes <ao_mendes@hotmail.com>
2026-03-17 10:28:54 -0300

    search function

--- app/routes/database/tables/reservas/reservas_equipamentos/handlers.py
+++ app/routes/database/tables/reservas/reservas_equipamentos/handlers.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import g, request
+from flask import flash, g, request
 from flask_sqlalchemy.pagination import SelectPagination
 from sqlalchemy import select
 
@@ -14,6 +14,7 @@ from app.enums import StatusReservaEquipamentoEnum
 from app.extensions import db
 from app.models.reservas.reservas_equipamentos import Reservas_Equipamentos
 from app.routes_helper.db_actions import db_action
+from app.routes_helper.request import get_query_params
 from config.general import PER_PAGE
 
 dispatcher = {}
@@ -35,7 +36,54 @@ def search_prefetch():
 
 @register_handler(dispatcher, 'procurar', 1)
 def search_fetch():
-    pass
+    id_reserva = none_if_empty(request.form.get('id_reserva'), int)
+    id_reserva_aula = none_if_empty(request.form.get('id_reserva_aula'), int)
+    id_reserva_responsavel = none_if_empty(request.form.get('id_reserva_responsavel'), int)
+    data_reserva = parse_date_string(request.form.get('data_reserva'))
+    estado = none_if_empty(request.form.get('estado'))
+    estado_enum = None
+    cancelado_por_id = none_if_empty(request.form.get('cancelado_por_id'), int)
+    cancelado_em = parse_datetime_string(request.form.get('cancelado_em'))
+    motivo_cancelamento = none_if_empty(request.form.get('motivo_cancelamento'))
+    try:
+        estado_enum = StatusReservaEquipamentoEnum(estado)
+    except ValueError as e:
+        estado_enum = None
+    filters = []
+    query_params = get_query_params(request)
+    if id_reserva:
+        filters.append(Reservas_Equipamentos.id_reserva == id_reserva)
+    if id_reserva_aula:
+        filters.append(Reservas_Equipamentos.id_reserva_aula == id_reserva_aula)
+    if id_reserva_responsavel:
+        filters.append(Reservas_Equipamentos.id_reserva_responsavel == id_reserva_responsavel)
+    if data_reserva:
+        filters.append(Reservas_Equipamentos.data_reserva == data_reserva)
+    if estado_enum:
+        filters.append(Reservas_Equipamentos.estado == estado_enum)
+    if cancelado_por_id:
+        filters.append(Reservas_Equipamentos.cancelado_por_id == cancelado_por_id)
+    if cancelado_em:
+        filters.append(Reservas_Equipamentos.cancelado_em == cancelado_em)
+    if motivo_cancelamento:
+        filters.append(Reservas_Equipamentos.motivo_cancelamento == motivo_cancelamento)
+    if filters:
+        sel_reservas = select(Reservas_Equipamentos).where(
+            *filters
+        )
+        reservas_paginadas = SelectPagination(
+            select=sel_reservas, session=db.session,
+            page=g.page, per_page=PER_PAGE, error_out=False
+        )
+        g.extras['reservas_equipamentos'] = reservas_paginadas.items
+        g.extras['pagination'] = reservas_paginadas
+        g.extras['query_params'] = query_params
+    else:
+        flash("especifique ao menos um campo", "danger")
+        g.redirect_action, g.bloco = register_return(g.url, g.acao, g.extras,
+            aulas_ativas = get_aulas_ativas(), pessoas = get_pessoas()
+    )
+
 
 @register_handler(dispatcher, 'inserir', 0)
 def insert_prefetch():
