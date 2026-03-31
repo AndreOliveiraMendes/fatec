5a61d73e8c2babadf1960a1201ef63b6092f92e6
Andre Oliveira Mendes <ao_mendes@hotmail.com>
2026-03-31 17:00:21 -0300

    feat: enhance equipment reservation management with detailed view and dynamic actions

--- app/routes/gestão_reservas/gestao_reserva_equipamento.py
+++ app/routes/gestão_reservas/gestao_reserva_equipamento.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, session
 
+from app.dao.internal.reservas import get_reservas_equipamentos
 from app.dao.internal.usuarios import get_user
 from app.decorators.decorators import admin_required
 
@@ -10,4 +11,10 @@ bp = Blueprint('gestao_reserva_equipamento', __name__, url_prefix='/gestao_reser
 def gerenciar_reservas_equipamentos():
     userid = session.get('userid')
     user = get_user(userid)
-    return render_template("gestão_reservas/reservas_equipamentos/gerenciar.html", user=user)
+
+    reservas = get_reservas_equipamentos()
+    return render_template(
+        "gestão_reservas/reservas_equipamentos/gerenciar.html",
+        user=user,
+        reservas=reservas
+    )
