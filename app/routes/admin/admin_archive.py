cdb45eb6aa3ce2bc074d9218ffffd34d18c8635a
Andre Oliveira Mendes <ao_mendes@hotmail.com>
2026-04-02 17:20:55 -0300

    feat: enhance admin archive menu with semester and year archiving functionality (WIP)

--- app/routes/admin/admin_archive.py
+++ app/routes/admin/admin_archive.py
@@ -1,11 +1,28 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, jsonify, render_template, session
 
 from app.dao.internal.usuarios import get_user
+from app.decorators.decorators import admin_required
+from app.routes.admin.handlers.handler_admin_archive import archive_by_semestre, archive_last_year_historicos
 
 bp = Blueprint("admin_archive", __name__, url_prefix='/admin/archive')
 
 @bp.route("/menu")
+@admin_required
 def menu():
     user = get_user(session.get('userid'))
 
     return render_template("admin/archive/menu.html", user=user)
+
+@bp.route("/semestre", methods=["POST"])
+@admin_required
+def archive_semestre():
+    result = archive_by_semestre()
+
+    return jsonify({"message": result})
+
+@bp.route("/ano", methods=["POST"])
+@admin_required
+def archive_ano():
+    result = archive_last_year_historicos()
+    return jsonify({"message": result})
+    
