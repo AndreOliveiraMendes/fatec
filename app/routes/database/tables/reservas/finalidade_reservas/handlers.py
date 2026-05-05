8b7cb82cc1968851fe531ae9392636bb055489ae
Andre Oliveira Mendes <ao_mendes@hotmail.com>
2026-05-05 15:50:59 -0300

    feat: add search functionality for finalidade_reservas with optional filters and update HTML template for search form

--- app/routes/database/tables/reservas/finalidade_reservas/handlers.py
+++ app/routes/database/tables/reservas/finalidade_reservas/handlers.py
@@ -1,8 +1,8 @@
 import json
 
-from flask import flash, g, request
+from flask import flash, g, request, session
 from flask_sqlalchemy.pagination import SelectPagination
-from sqlalchemy import select
+from sqlalchemy import func, select
 
 from app.auxiliar.general import none_if_empty
 from app.auxiliar.navigation import register_return
@@ -10,7 +10,8 @@ from app.decorators.decorators import register_handler
 from app.extensions import db
 from app.models.reservas.reservas_laboratorios import Finalidade_Reserva
 from app.routes_helper.db_actions import db_action
-from config.general import PER_PAGE
+from app.routes_helper.request import get_query_params
+from config.general import PER_PAGE, str_to_bool
 
 
 dispatcher = {}
@@ -24,6 +25,44 @@ def listar_handler():
     )
     g.extras['finalidades'] = finalidade_reservas_paginada.items
     g.extras['pagination'] = finalidade_reservas_paginada
+
+@register_handler(dispatcher, 'procurar', 1)
+def search_handler():
+    id_finalidade = none_if_empty(request.form.get('id_finalidade'), int)
+    nome = none_if_empty(request.form.get('nome'))
+    ativo = none_if_empty(request.form.get('ativo'), str_to_bool)
+    descricao = none_if_empty(request.form.get('descricao'))
+    template = none_if_empty(request.form.get('config_template'))
+
+    filters = []
+    query_params = get_query_params(request)
+    if id_finalidade is not None:
+        filters.append(Finalidade_Reserva.id_finalidade == id_finalidade)
+    if nome:
+        filters.append(Finalidade_Reserva.nome.ilike(f"%{nome}%"))
+    if ativo is not None:
+        print(request.form.get('ativo'), bool(ativo))
+        filters.append(Finalidade_Reserva.ativo == ativo)
+    if descricao:
+        filters.append(Finalidade_Reserva.descricao.ilike(f"%{descricao}%"))
+    if template:
+        filters.append(
+            func.JSON_UNQUOTE(
+                func.JSON_EXTRACT(Finalidade_Reserva.config, '$.template')
+            ).ilike(f"%{template}%")
+        )
+    if filters:
+        sel_finalidades = select(Finalidade_Reserva).where(*filters)
+        finalidade_reservas_paginada = SelectPagination(
+            select=sel_finalidades, session=db.session,
+            page=g.page, per_page=PER_PAGE, error_out=False
+        )
+        g.extras['finalidades'] = finalidade_reservas_paginada.items
+        g.extras['pagination'] = finalidade_reservas_paginada
+        g.extras['query_params'] = query_params
+    else:
+        flash("especifique ao menos um campo", "danger")
+        g.redirect_action, g.bloco = register_return(g.url, g.acao, g.extras)
 
 @register_handler(dispatcher, 'inserir', 1)
 def insert_push():
