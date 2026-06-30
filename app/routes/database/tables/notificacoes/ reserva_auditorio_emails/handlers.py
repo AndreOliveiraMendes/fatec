29033a1ba9b223708a11571e8e02852712b419da
Andre <ao_mendes@hotmail.com>
2026-06-30 16:56:32 -0300

    feat: enhance email reservation form to include status_envio, data_envio, erro_envio, tentativas, and ultima_tentativa fields

--- app/routes/database/tables/notificacoes/ reserva_auditorio_emails/handlers.py
+++ app/routes/database/tables/notificacoes/ reserva_auditorio_emails/handlers.py
@@ -1,5 +1,7 @@
-from flask import g
+from flask import g, request
 
+from app.auxiliar.general import get_value_or_abort, none_if_empty
+from app.auxiliar.parsing import parse_datetime_string
 from app.dao.internal.reservas import get_reservas_auditorios_database
 from app.decorators.decorators import register_handler
 
@@ -10,5 +12,13 @@ def insert_prefetch():
     g.extras['reservas_auditorios'] = get_reservas_auditorios_database()
 
 @register_handler(dispatcher, 'inserir', 1)
-def insert_fetch():
-    pass
+def insert_push():
+    id_reserva_auditorio = get_value_or_abort(request.form.get('id_reserva_auditorio'), 400, "id_reserva_auditorio é obrigatorio", int)
+    destinatario = get_value_or_abort(request.form.get('destinatario'), 400, "destinatário é obrigatorio")
+    assunto = get_value_or_abort(request.form.get('assunto'), 400, "assunto é obrigatorio")
+    conteudo = get_value_or_abort(request.form.get('conteudo'), 400, "conteudo é obrigatorio")
+    status_envio = get_value_or_abort(request.form.get('status_envio'), 400, "status_envio é obrigatorio")
+    data_envio = parse_datetime_string(request.form.get('data_envio'))
+    erro_envio = none_if_empty(request.form.get('erro_envio'))
+    tentativas = get_value_or_abort(request.form.get('tentativas'), 400, "tentativas é obrigatorio", int)
+    ultima_tentativa = parse_datetime_string(request.form.get('ultima_tentativa'))
