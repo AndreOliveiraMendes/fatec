25acbfd419f588b5c54b769b24974e6f37880e93
Andre <ao_mendes@hotmail.com>
2026-08-18 15:59:47 -0300

    feat: add email configuration activation and deactivation functionality; implement test email feature and update admin panel interactions

--- app/routes/api/handler/handler_mail_config.py
+++ app/routes/api/handler/handler_mail_config.py
@@ -1,3 +1,13 @@
+import smtplib
+
+TEXT_MAIL = f"""
+Olá, este é um email de teste enviado pelo sistema.
+se você recebeu este email, significa que a configuração de email está funcionando corretamente.
+caso você não tenha solicitado este email, por favor, ignore-o.
+
+atenciosamente, sistema de gerenciamento de reservas de recursos.
+"""
+
 def same_config(config, data):
     # Compare the relevant fields to determine if they are the same configuration
     return (
@@ -6,4 +16,25 @@ def same_config(config, data):
         config.get('username') == data.get('username') and
         config.get('mail_from') == data.get('mail_from') and
         config.get('use_tls') == data.get('use_tls')
-    )
+    ) or config.get('id') == data.get('id')
+
+def get_config_by_id(configs, config_id):
+    mail_configs = configs.get("configs", [])
+    return next((config for config in mail_configs if config.get("id") == config_id), None)
+
+def send_test_email(smtp_server, smtp_port, username, password, mail_from, mail_to, use_tls=True, subject="Test Email", body=TEXT_MAIL):
+    try:
+        # Create the email headers and body
+        message = f"Subject: {subject}\n\n{body}"
+
+        # Connect to the SMTP server
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            if use_tls:
+                server.starttls()  # Upgrade the connection to a secure encrypted SSL/TLS connection
+            server.login(username, password)  # Log in to the SMTP server
+            server.sendmail(mail_from, mail_to, message)  # Send the email
+
+        return True  # Email sent successfully
+    except Exception as e:
+        print(f"Error sending test email: {e}")
+        return False  # Email sending failed
--- app/routes/api/mail.py
+++ app/routes/api/mail.py
@@ -1,23 +1,43 @@
 from flask import Blueprint, request
-from app.security.cryptograph import encrypt_field
+from app.decorators.decorators import admin_required
+from app.security.cryptograph import decrypt_field, encrypt_field
 from config.json_related import load_mail_config, save_mail_config
 
-from .handler.handler_mail_config import same_config
+from .handler.handler_mail_config import get_config_by_id, same_config, send_test_email
 
 bp = Blueprint('api_mail', __name__, url_prefix='/api/mail')
 
 @bp.route("/list")
+@admin_required
 def api_mail_list():
     configs = load_mail_config()
-    return {"configs": configs.get("configs", [])}
+    return {
+        "configs": configs.get("configs", []),
+        "active": configs.get("active", 0)
+    }
+
+@bp.route("/get/<int:config_id>")
+@admin_required
+def api_mail_get(config_id):
+    configs = load_mail_config()
+    config = get_config_by_id(configs, config_id)
+    if not config:
+        return {"error": "Configuração não encontrada."}, 404
+    return {"config": config}
 
 @bp.route("/save", methods=["POST"])
+@admin_required
 def api_mail_save():
     data = request.get_json()
     configs = load_mail_config()
     mail_configs = configs.get("configs", [])
     if not data:
         return {"error": "No data provided"}, 400
+    if data.get('id'):
+        try:
+            data['id'] = int(data['id'])
+        except ValueError:
+            return {"error": "Invalid ID format"}, 400
 
     # Check if the configuration already exists
     existing_config = next((config for config in mail_configs if same_config(config, data)), None)
@@ -29,6 +49,8 @@ def api_mail_save():
         # Update the existing configuration
         if not data.get('credential'):
             data['credential'] = existing_config['credential']
+        if not data.get('id'):
+            data['id'] = existing_config['id']
         existing_config.update(data)
     else:
         # generate a new ID for the new configuration
@@ -38,13 +60,81 @@ def api_mail_save():
     save_mail_config(configs)
     return {"success": True}
 
+@bp.route("/active/<int:config_id>", methods=["POST"])
+@admin_required
+def api_mail_active(config_id):
+    configs = load_mail_config()
+
+    active = configs.get("active", 0)
+
+    config = get_config_by_id(configs, config_id)
+
+    if config is None:
+        return {"error": "Configuração não encontrada."}, 404
+
+    if active == config_id:
+        return {"error": "Configuração já está ativa."}, 400
+
+    configs["active"] = config_id
+
+    save_mail_config(configs)
+
+    return {"success": True}
+
 @bp.route("/delete/<int:config_id>", methods=["POST"])
+@admin_required
 def api_mail_delete(config_id):
     configs = load_mail_config()
+    active = configs.get("active", 0)
     mail_configs = configs.get("configs", [])
     new_mail_configs = [config for config in mail_configs if config.get('id') != config_id]
     if len(new_mail_configs) == len(mail_configs):
         return {"error": "Configuration not found"}, 404
+    if active == config_id:
+        configs["active"] = None
     configs["configs"] = new_mail_configs
     save_mail_config(configs)
     return {"success": True}
+
+@bp.route("/desactive/<int:config_id>", methods=["POST"])
+@admin_required
+def api_mail_desactive(config_id):
+    configs = load_mail_config()
+    active = configs.get("active", None)
+    if active is None:
+        return {"error": "Already desactivated"}, 404
+    if active != config_id:
+        return {"error": "Configuration is not active"}, 400
+    configs["active"] = None
+    save_mail_config(configs)
+    return {"success": True}
+
+@bp.route("/test/<int:config_id>", methods=["POST"])
+@admin_required
+def api_mail_test(config_id):
+    configs = load_mail_config()
+    config = get_config_by_id(configs, config_id)
+    if not config:
+        return {"error": "Configuração não encontrada."}, 404
+
+    data = request.get_json()
+    email = data.get("email")
+    if not email:
+        return {"error": "Email não fornecido."}, 400
+
+    if config.get("auth_type") == "app_password" and not config.get("credential"):
+        return {"error": "Credencial não fornecida para autenticação."}, 400
+
+    if config.get("auth_type") == "app_password":
+        config["credential"] = decrypt_field(config["credential"])
+
+        mail_sent = send_test_email(
+            smtp_server=config.get("host"),
+            smtp_port=config.get("port"),
+            username=config.get("user"),
+            password=config.get("credential"),
+            mail_from=config.get("mail_from"),
+            mail_to=email,
+            use_tls=config.get("use_tls", True),
+            subject="Teste de Configuração de Email"
+        )
